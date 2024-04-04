bb0cd2dc2f3b23af1c0e749625f98c35c99a6943
PierluigiPapa <pierluigipapa9@gmail.com>
2024-04-04 12:43:51 +0200

    feat: rename HelloWorld.vue to AppMain.vue and add axios on AppMain.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name:'AppMain',
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+    getProjects(){
+
+      axios.get('http://127.0.0.1:8000/api/test')
+      .then(res => {
+        console.log(res)
+      })
+    }
+  },
+  mounted() {
+    
+  },
+}
+</script>
+
+<template>
+  <h3>Cliccate qua per vedere i progetti:</h3>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
